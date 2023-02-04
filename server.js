1dc20b060f3e8729d7256c2ec47367bdc0210ae6
krksaikumar <techpvec23@gmail.com>
2023-02-04 21:25:05 +0530

    made changes for vercel

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const puppeteer = require('puppeteer')
+// const puppeteer = require('puppeteer')
 const dotenv = require('dotenv').config()
 
 const app = express()
@@ -7,9 +7,29 @@ const PORT = process.env.PORT || 5000
 function sleep(seconds, fun = () => {}) {
     return new Promise(r => setTimeout(()=>{fun(); r()}, seconds * 1000))
 }
+
+let chrome = {};
+let puppeteer;
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    chrome = require("chrome-aws-lambda");
+    puppeteer = require("puppeteer-core");
+} else {
+    puppeteer = require("puppeteer");
+}
+
 app.get('/api/start', async (req, res) => {
+    let options = {};
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+          args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+          defaultViewport: chrome.defaultViewport,
+          executablePath: await chrome.executablePath,
+          headless: true,
+          ignoreHTTPSErrors: true,
+        };
+    }
     try {
-        const browser = await puppeteer.launch({headless: false})
+        const browser = await puppeteer.launch(options);
         const page = await browser.newPage()
         await page.goto('https://kaggle.com')
         
